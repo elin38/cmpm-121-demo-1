19e99af05b88133dd518d643acec231709585107
Ethan Lin <eragon9356@gmail.com>
2024-10-08 23:48:49 -0700

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,11 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
-
+// step 1
 const button = document.createElement("button");
 button.innerHTML = "🎼";
 app.append(button);
-
+// step 2
 let counter: number = 0;
 
 const displayCounter = document.createElement("div");
@@ -23,3 +23,10 @@ button.addEventListener("click", () => {
     counter += 1;
     displayCounter.innerHTML = `${counter} notes`;
 });
+// step 3
+setInterval(increaseCounter, 1000, 1);
+
+function increaseCounter(amount: number) {
+    counter += amount;
+    displayCounter.innerHTML = `${counter} notes`;
+}
